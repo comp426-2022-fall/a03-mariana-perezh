9281af302be2a02b6f3d191194e1b6cd786d75dc
mariana-perezh <mperezh@email.unc.edu>
2022-11-15 10:18:37 -0500

    bin and lib changes

--- bin/cli.js
+++ bin/cli.js
@@ -2,14 +2,14 @@
 
 import minimist form 'minimist';
 
-import {roll} from "/lib/roll.js";
+import roll from "../lib/roll.js";
 
 const args = minimist(process.argv.slice(2));
 
 const sides = args.sides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
-const results = roll(sides, dice, rolls);
+const results = {sides: sides, dice: dice, rolls: roll(sides, dice, rolls)};
 
 
-JSON.stringify(sides, dice, rolls, results);
+console.log(JSON.stringify(results));
