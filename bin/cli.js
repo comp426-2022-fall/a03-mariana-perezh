b3a0cd9e6d39c91535ab787cc4f63fc9fbc5f91c
mariana-perezh <mperezh@email.unc.edu>
2022-11-15 10:34:10 -0500

    works?

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import minimist form 'minimist';
+import minimist from 'minimist';
 
 import roll from "../lib/roll.js";
 
@@ -9,7 +9,8 @@ const args = minimist(process.argv.slice(2));
 const sides = args.sides || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
-const results = {sides: sides, dice: dice, rolls: roll(sides, dice, rolls)};
+const results = roll(sides, dice, rolls);
+
 
 
 console.log(JSON.stringify(results));
